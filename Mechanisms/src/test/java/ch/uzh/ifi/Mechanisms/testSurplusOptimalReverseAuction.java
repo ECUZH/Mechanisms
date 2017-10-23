56baa7e743f93f88b9e6e5483e30b85780eb5f66
dmitrymoor <dmitry.moor@yandex.ru>
2017-10-23 14:38:18 +0200

    +1 unittest

--- Mechanisms/src/test/java/ch/uzh/ifi/Mechanisms/testSurplusOptimalReverseAuction.java
+++ Mechanisms/src/test/java/ch/uzh/ifi/Mechanisms/testSurplusOptimalReverseAuction.java
@@ -13,9 +13,13 @@ import ch.uzh.ifi.MechanismDesignPrimitives.Distribution;
 import ch.uzh.ifi.MechanismDesignPrimitives.SellerType;
 import ch.uzh.ifi.MechanismDesignPrimitives.Type;
 
-public class testSurplusOptimalReverseAuction {
+public class testSurplusOptimalReverseAuction 
+{
 
-	
+
+	/**
+	 * In this scenario bidders are i.i.d. and the payment is equal to the reserve price.
+	 */
 	@Test
 	public void testIIDReservePrice() 
 	{
@@ -46,6 +50,9 @@ public class testSurplusOptimalReverseAuction {
 		assertTrue( (auction.computePayments().get(0) - 1.6) < 1e-6);		// payment = reserve price
 	}
 
+	/**
+	 * In this scenario bidders are i.i.d. and the payment is equal to the second lowest bid.
+	 */
 	@Test
 	public void testIIDSecondPrice() 
 	{
@@ -73,6 +80,36 @@ public class testSurplusOptimalReverseAuction {
 		assertTrue( allocation.getBiddersInvolved(0).size() == 1);
 		assertTrue( allocation.getBiddersInvolved(0).get(0) == 1);
 		assertTrue( auction.computePayments().size() == 1 );
-		assertTrue( (auction.computePayments().get(0) - 2.) < 1e-6);		// payment = reserve price
+		assertTrue( (auction.computePayments().get(0) - 2.) < 1e-6);		// payment = second price
+	}
+	
+	@Test
+	public void testSecondPrice() 
+	{
+		int dbID1 = 1;
+		int sellerId1 = 1;
+		double sellerCost1 = 0.8;
+		List<Integer> bundle1 = Arrays.asList(dbID1);
+		AtomicBid atom1 = new AtomicBid(sellerId1, bundle1, sellerCost1);
+		SellerType seller1 = new SellerType(atom1, Distribution.UNIFORM, 1., 1./3.);
+		
+		int sellerId2 = 2;
+		double sellerCost2 = 1.;
+		List<Integer> bundle2 = Arrays.asList(dbID1);
+		AtomicBid atom2 = new AtomicBid(sellerId2, bundle2, sellerCost2);
+		
+		SellerType seller2 = new SellerType(atom2, Distribution.UNIFORM, 0.5, 1./12.);
+
+		List<Type> bids = Arrays.asList(seller1, seller2);
+		
+		double auctioneerValue = 10.;
+		SurplusOptimalReverseAuction auction = new SurplusOptimalReverseAuction(bids.size(), bids, auctioneerValue);
+		auction.computeWinnerDetermination();
+		Allocation allocation = auction.getAllocation();
+		
+		assertTrue( allocation.getBiddersInvolved(0).size() == 1);
+		assertTrue( allocation.getBiddersInvolved(0).get(0) == 1);
+		assertTrue( auction.computePayments().size() == 1 );
+		assertTrue( (auction.computePayments().get(0) - 1.) < 1e-6);		// payment = second price
 	}
 }
