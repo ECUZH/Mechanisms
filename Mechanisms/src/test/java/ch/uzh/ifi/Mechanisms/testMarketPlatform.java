6fe258512b37732e50dd8b71533a6ecde5ff894f
dmitrymoor <dmitry.moor@yandex.ru>
2017-10-23 19:31:56 +0200

    test for the price finding algorithm

--- Mechanisms/src/test/java/ch/uzh/ifi/Mechanisms/testMarketPlatform.java
+++ Mechanisms/src/test/java/ch/uzh/ifi/Mechanisms/testMarketPlatform.java
@@ -9,10 +9,12 @@ import java.util.List;
 import org.junit.Test;
 
 import ch.uzh.ifi.MechanismDesignPrimitives.AtomicBid;
+import ch.uzh.ifi.MechanismDesignPrimitives.Distribution;
 import ch.uzh.ifi.MechanismDesignPrimitives.IParametrizedValueFunction;
 import ch.uzh.ifi.MechanismDesignPrimitives.LinearThresholdValueFunction;
 import ch.uzh.ifi.MechanismDesignPrimitives.ParametrizedQuasiLinearAgent;
 import ch.uzh.ifi.MechanismDesignPrimitives.ProbabilisticAllocation;
+import ch.uzh.ifi.MechanismDesignPrimitives.SellerType;
 
 public class testMarketPlatform {
 
@@ -21,6 +23,7 @@ public class testMarketPlatform {
 	 * Different sellers produce different DBs.
 	 * @throws Exception 
 	 */
+	/*
 	@Test
 	public void testMarketDemandWithLargeEndowments() throws Exception 
 	{
@@ -35,10 +38,13 @@ public class testMarketPlatform {
 		double cost1 = 10;
 		double cost2 = 20;
 		
-		AtomicBid seller1 = new AtomicBid(1, bundle1, cost1);
-		AtomicBid seller2 = new AtomicBid(2, bundle2, cost2);
+		AtomicBid sellerBid1 = new AtomicBid(1, bundle1, cost1);
+		AtomicBid sellerBid2 = new AtomicBid(2, bundle2, cost2);
 		
-		List<AtomicBid> sellers = Arrays.asList(seller1, seller2);
+		SellerType seller1 = new SellerType(sellerBid1, Distribution.UNIFORM, 1., 1./3.);
+		SellerType seller2 = new SellerType(sellerBid2, Distribution.UNIFORM, 1., 1./3.);
+		
+		List<SellerType> sellers = Arrays.asList(seller1, seller2);
 		
 		
 		//2. Create 2 buyers
@@ -85,8 +91,8 @@ public class testMarketPlatform {
 		
 		double auctioneerValue = 0;
 		List<Double> biddersValues = new LinkedList<Double>();
-		biddersValues.add(seller1.getValue());
-		biddersValues.add(seller2.getValue());
+		biddersValues.add(seller1.getAtom(0).getValue());
+		biddersValues.add(seller2.getAtom(0).getValue());
 		
 		ProbabilisticAllocation allocation = new ProbabilisticAllocation();				//Probabilistic allocation of sellers
 		List<Double> allocationProbabilities = new LinkedList<Double>();
@@ -141,6 +147,147 @@ public class testMarketPlatform {
 		
 		assertTrue(Math.abs( mp.computeValueOfDB(dbID1, 1 - 1e-8, allocation)  - 4) < 1e-6);
 		assertTrue(Math.abs( mp.computeValueOfDB(dbID2, 1 - 1e-8, allocation)  - 4) < 1e-6);
-	}
+	}*/
 
+	
+	/**
+	 * There are 2 sellers and 2 buyers in this scenario (see AAMAS'17 paper).
+	 * Different sellers produce different DBs.
+	 * @throws Exception 
+	 */
+	@Test
+	public void testMarketDemandWithLargeEndowments() throws Exception 
+	{
+		//0. Define DBs
+		int dbID1 = 0;
+		int dbID2 = 1;
+		
+		//1. Create 2 sellers
+		List<Integer> bundle1 = Arrays.asList(dbID1);
+		List<Integer> bundle2 = Arrays.asList(dbID2);
+		
+		double cost1 = 1.5;
+		double cost2 = 2.;
+		double cost3 = 1.5;
+		
+		AtomicBid sellerBid1 = new AtomicBid(1, bundle1, cost1);
+		AtomicBid sellerBid2 = new AtomicBid(2, bundle2, cost2);
+		AtomicBid sellerBid3 = new AtomicBid(3, bundle2, cost3);
+		
+		SellerType seller1 = new SellerType(sellerBid1, Distribution.UNIFORM, 1., 1./3.);
+		SellerType seller2 = new SellerType(sellerBid2, Distribution.UNIFORM, 1., 1./3.);
+		SellerType seller3 = new SellerType(sellerBid3, Distribution.UNIFORM, 1., 1./3.);
+		
+		List<SellerType> sellers = Arrays.asList(seller1, seller2, seller3);
+		
+		
+		//2. Create 2 buyers
+		double endowment = 10;
+		int allocations[] = {0b00, 0b01, 0b10, 0b11};									// 4 possible deterministic allocations of DBs
+		
+		double[] alloc1 = {0,0};
+		IParametrizedValueFunction v11 = new LinearThresholdValueFunction(0, 0, alloc1);
+		IParametrizedValueFunction v21 = new LinearThresholdValueFunction(0, 0, alloc1);
+		
+		double[] alloc2 = {0,1};
+		IParametrizedValueFunction v12 = new LinearThresholdValueFunction(4, 1, alloc2);
+		IParametrizedValueFunction v22 = new LinearThresholdValueFunction(1, 2, alloc2);
+		
+		double[] alloc3 = {1,0};
+		IParametrizedValueFunction v13 = new LinearThresholdValueFunction(4, 1, alloc3);
+		IParametrizedValueFunction v23 = new LinearThresholdValueFunction(1, 2, alloc3);
+		
+		double[] alloc4 = {1,1};
+		IParametrizedValueFunction v14 = new LinearThresholdValueFunction(6, 1, alloc4);
+		IParametrizedValueFunction v24 = new LinearThresholdValueFunction(1, 4, alloc4);
+		
+		IParametrizedValueFunction[] valueFunctions1 = {v11, v12, v13, v14};			//Parameterized value functions for both buyers 
+		IParametrizedValueFunction[] valueFunctions2 = {v21, v22, v23, v24};
+		ParametrizedQuasiLinearAgent buyer1 = new ParametrizedQuasiLinearAgent(1, endowment, allocations, valueFunctions1);
+		ParametrizedQuasiLinearAgent buyer2 = new ParametrizedQuasiLinearAgent(2, endowment, allocations, valueFunctions2);
+		
+		List<ParametrizedQuasiLinearAgent> buyers = new LinkedList<ParametrizedQuasiLinearAgent>();
+		buyers.add(buyer1);
+		buyers.add(buyer2);
+		
+		//3. Create market platform and evaluate the market demand
+		MarketPlatform mp = new MarketPlatform(buyers, sellers);
+		
+		double price  = mp.tatonementPriceSearch();
+		
+		System.out.println("The resulting price is " + price);
+		/*int auctioneerId = 0; 															//The market platform, M
+		
+		List<Integer> bidders = new LinkedList<Integer>();
+		bidders.add(seller1.getAgentId());
+		bidders.add(seller2.getAgentId());
+		bidders.add(seller3.getAgentId());
+		
+		List<Integer> bundles = new LinkedList<Integer>();
+		bundles.add(dbID1);																//Id of the bundle allocated to the 1st bidder
+		bundles.add(dbID2);																//Id of the bundle allocated to the 2nd bidder
+		bundles.add(dbID2);																//Id of the bundle allocated to the 3rd bidder
+		
+		double auctioneerValue = 0;
+		List<Double> biddersValues = new LinkedList<Double>();
+		biddersValues.add(seller1.getAtom(0).getValue());
+		biddersValues.add(seller2.getAtom(0).getValue());
+		biddersValues.add(seller3.getAtom(0).getValue());
+		
+		ProbabilisticAllocation allocation = new ProbabilisticAllocation();				//Probabilistic allocation of sellers
+		List<Double> allocationProbabilities = new LinkedList<Double>();
+		allocationProbabilities.add(1.0);
+		allocationProbabilities.add(1.0);
+		
+		allocation.addAllocatedAgent(auctioneerId, bidders, bundles, auctioneerValue, biddersValues, allocationProbabilities);*/
+		
+		
+		
+		/*List<Double> marketDemand = mp.computeMarketDemand(0., allocation);
+		
+		assertTrue(Math.abs( marketDemand.get(1) - 3. ) < 1e-6);
+		assertTrue(Math.abs( marketDemand.get(0) - 20. ) < 1e-6);
+		
+		marketDemand = mp.computeMarketDemand(1. - 1e-8, allocation);
+		assertTrue(Math.abs( marketDemand.get(1) - 3. ) < 1e-6);
+		assertTrue(Math.abs( marketDemand.get(0) - 17. ) < 1e-6);
+		
+		marketDemand = mp.computeMarketDemand(1. + 1e-8, allocation);
+		assertTrue(Math.abs( marketDemand.get(1) - 1. ) < 1e-6);
+		assertTrue(Math.abs( marketDemand.get(0) - 19. ) < 1e-6);
+		
+		marketDemand = mp.computeMarketDemand(4. - 1e-8, allocation);
+		assertTrue(Math.abs( marketDemand.get(1) - 1. ) < 1e-6);
+		assertTrue(Math.abs( marketDemand.get(0) - 16. ) < 1e-6);
+		
+		marketDemand = mp.computeMarketDemand(4. + 1e-8, allocation);
+		assertTrue(Math.abs( marketDemand.get(1) - 0. ) < 1e-6);
+		assertTrue(Math.abs( marketDemand.get(0) - 20. ) < 1e-6);
+		
+		//4. Test aggregate value function
+		
+		assertTrue(Math.abs( mp.computeAggregateValue(123, allocation)-6 ) < 1e-6);
+		assertTrue(Math.abs( mp.computeAggregateValue(3+ 1e-8, allocation)-6 ) < 1e-6);
+		assertTrue(Math.abs( mp.computeAggregateValue(3- 0.1, allocation)-(5.9) ) < 1e-6);
+		assertTrue(Math.abs( mp.computeAggregateValue(1+ 0.1, allocation)-(4.1) ) < 1e-6);
+		assertTrue(Math.abs( mp.computeAggregateValue(1- 0.1, allocation)-(3.6) ) < 1e-6);
+		assertTrue(Math.abs( mp.computeAggregateValue(0.5, allocation)-(2) ) < 1e-6);
+		assertTrue(Math.abs( mp.computeAggregateValue(0, allocation)-0 ) < 1e-6);
+		
+		//5. Test values of DBs
+		assertTrue(Math.abs( mp.computeValueOfDB(dbID1, 1 - 1e-8, allocation)  - 0) < 1e-6);
+		assertTrue(Math.abs( mp.computeValueOfDB(dbID2, 1 - 1e-8, allocation)  - 6) < 1e-6);
+		
+		allocation.deallocateBundle(dbID1);
+		allocation.deallocateBundle(dbID2);
+		
+		assertTrue(Math.abs( mp.computeValueOfDB(dbID1, 1 - 1e-8, allocation)  - 0) < 1e-6);
+		assertTrue(Math.abs( mp.computeValueOfDB(dbID2, 1 - 1e-8, allocation)  - 0) < 1e-6);
+		
+		allocationProbabilities = Arrays.asList(1., 1.);
+		allocation.resetAllocationProbabilities(allocationProbabilities);
+		
+		assertTrue(Math.abs( mp.computeValueOfDB(dbID1, 1 - 1e-8, allocation)  - 4) < 1e-6);
+		assertTrue(Math.abs( mp.computeValueOfDB(dbID2, 1 - 1e-8, allocation)  - 4) < 1e-6);*/
+	}
 }
