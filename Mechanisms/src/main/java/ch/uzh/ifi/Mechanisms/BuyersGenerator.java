ed672e582a43831187dd717ba858f65370f8be8d
dmitrymoor <dmitry.moor@yandex.ru>
2018-03-19 14:52:15 +0100

    upd

--- Mechanisms/src/main/java/ch/uzh/ifi/Mechanisms/BuyersGenerator.java
+++ Mechanisms/src/main/java/ch/uzh/ifi/Mechanisms/BuyersGenerator.java
@@ -1,68 +1,145 @@
 package ch.uzh.ifi.Mechanisms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import ch.uzh.ifi.MechanismDesignPrimitives.IParametrizedValueFunction;
 import ch.uzh.ifi.MechanismDesignPrimitives.LinearThresholdValueFunction;
 import ch.uzh.ifi.MechanismDesignPrimitives.ParametrizedQuasiLinearAgent;
 
+/**
+ * The class implements the functionality for generating buyers' value functions in the combinatorial 
+ * data market setting. 
+ * @author Dmitry Moor
+ *
+ */
 public class BuyersGenerator 
 {
 
 	/**
 	 * Constructor
-	 * @param numberOfDBs
+	 * @param numberOfDBs number of data bases in the domain
+	 * @param endowment initial endowment of buyers with money
+	 * @param seed random seed
 	 */
-	public BuyersGenerator(int numberOfDBs, double endowment)
+	public BuyersGenerator(int numberOfDBs, double endowment, int seed)
 	{
 		_numberOfDBs = numberOfDBs;
 		_endowment = endowment;
+		_seed = seed;
 	}
 	
 	/**
-	 * The method generates a new buyer.
-	 * @return
+	 * The method generates a new buyer. 
+	 * @param id agent id
+	 * @return a newly generated buyer
 	 */
 	public ParametrizedQuasiLinearAgent generateBuyer(int id)
 	{
-		int nDeterministicAllocations = (int)Math.pow(2, _numberOfDBs); 
+		int nDeterministicAllocations = (int)Math.pow(2, _numberOfDBs); // Number of different deterministic allocations of DBs
 		int allocations[] = new int[nDeterministicAllocations];			// Possible deterministic allocations of DBs
-		LinearThresholdValueFunction[] valueFunctions = new LinearThresholdValueFunction[nDeterministicAllocations];
+		//LinearThresholdValueFunction[] valueFunctions = new LinearThresholdValueFunction[nDeterministicAllocations];
+		Map<Integer, LinearThresholdValueFunction> valueFunctions = new HashMap<Integer, LinearThresholdValueFunction>();
 		
-		for(int i = 0; i < nDeterministicAllocations; ++i)				// Binary encodings of different allocations
-		{	
-			allocations[i] = i;
+		for(int i = 0; i < nDeterministicAllocations; ++i)				// Binary encodings of different allocations, e.g., ... 
+		{																// ... for 2 databases db1 and db2 possible deterministic ...
+			allocations[i] = i;											// ...  allocations are 00, 01, 10, 11.
 			
-			double[] alloc = new double[_numberOfDBs];					// Convert binary representation from int to double[]
-			for(int j = 0; j < _numberOfDBs; ++j)
-				if( ((i >> j) & 0b1 ) > 0 )
-					alloc[_numberOfDBs - 1 - j] = 1;
-				else 
-					alloc[_numberOfDBs - 1 - j] = 0;
+			List<Double> alloc = convertToArray(i);
 			
-			int nAllocated = (int)Arrays.stream(alloc).sum();			// Number of allocated DBs
+			int nAllocated = (int)alloc.stream().mapToDouble( v -> v ).sum();			// Number of allocated DBs
+			int magicNumber1 = 1013;
+			int magicNumber2 = 11;
+			Random gen = new Random(_seed * 100000 + id * magicNumber1 + /*nAllocated*/ i * magicNumber2 );
 			
 			int threshold = 0;
 			double marginalValue = 0.;
 			if(i > 0)
 			{
 				int idxPrev = (int)Math.pow(2, nAllocated-1) - 1;
-				marginalValue = _valFactor * Math.random() + valueFunctions[idxPrev].getMarginalValue();
-				threshold = (int)(valueFunctions[idxPrev].getMarginalValue() * valueFunctions[idxPrev].getThreshold() / marginalValue + 
-						_thresholdFactor * Math.random());
+				List<Integer> subsetKeys = getSubsets(allocations[i]);
+				
+				double maxMarginalValue = subsetKeys.stream().map( k -> valueFunctions.get(k).getMarginalValue()).mapToDouble(v -> v).max().orElse(0.);
+				double maxThreshold = maxMarginalValue != 0 ? subsetKeys.stream().map( k -> valueFunctions.get(k).getMarginalValue() * valueFunctions.get(k).getThreshold()).mapToDouble( v -> v ).max().orElse(0.) / maxMarginalValue : 0.;
+				
+				marginalValue = _valFactor * gen.nextDouble() + marginalValue;
+				threshold = (int) Math.floor(maxThreshold + _thresholdFactor * gen.nextDouble());
 			}
 			
 			LinearThresholdValueFunction v = new LinearThresholdValueFunction(marginalValue, threshold, alloc);
-			valueFunctions[i] = v;
+			valueFunctions.put(allocations[i], v);
 		}
 		
 		// Parameterized value functions for the buyer
-		ParametrizedQuasiLinearAgent buyer = new ParametrizedQuasiLinearAgent(id, _endowment, allocations, valueFunctions);
+		ParametrizedQuasiLinearAgent buyer = new ParametrizedQuasiLinearAgent(id, _endowment, valueFunctions);
 		return buyer;
+	}
+	
+	
+	/**
+	 * The method returns binary encodings of the given allocation subsets of size N-1, 
+	 * where N is the number of allocated DBs in the given binary allocation.
+	 * @param binaryAllocation binary representation of the allocation
+	 * @return a list of binary sub-allocations
+	 */
+	private List<Integer> getSubsets(int binaryAllocation)
+	{
+		List<Integer> subsetsKeys = new LinkedList<Integer>();
+		List<Double> alloc = convertToArray(binaryAllocation);
+		
+		for(int k = 0; k < _numberOfDBs; ++k)
+		{
+			List<Double> subsetK = new ArrayList<Double>();
+			for( Double v : alloc)
+				subsetK.add(v);
+			
+			subsetK.set(k, 0.);
+			
+			int binarySubset = convertToBinary(subsetK);
+			
+			if( binarySubset != binaryAllocation )
+				subsetsKeys.add( binarySubset );
+		}
+		
+		return subsetsKeys;
+	}
+	
+	
+	private List<Double> convertToArray(int binaryAllocation)
+	{
+		List<Double> alloc = new ArrayList<Double>(_numberOfDBs);
+		
+		for(int j = 0; j < _numberOfDBs; ++j)
+			alloc.add(0.);
+		
+		for(int j = 0; j < _numberOfDBs; ++j)
+			if( ((binaryAllocation >> j) & 0b1 ) > 0 )
+				alloc.set(_numberOfDBs - 1 - j, 1.);
+			else 
+				alloc.set(_numberOfDBs - 1 - j, 0.);
+		
+		return alloc;
+	}
+	
+	
+	private int convertToBinary(List<Double> alloc)
+	{
+		int binaryAllocation = 0;
+		for(int i = 0; i < alloc.size(); ++i)
+			binaryAllocation += (int) (alloc.get(i) * Math.pow(2, _numberOfDBs - i - 1));
+		
+		return binaryAllocation;
 	}
 	
 	private int _numberOfDBs;					// Number of DBs
 	private double _endowment;					// Endowment of the buyer
 	private double _valFactor = 2.;
-	private double _thresholdFactor = 2.;
+	private double _thresholdFactor = 5.;
+	private int _seed;							// Random seed
 }
